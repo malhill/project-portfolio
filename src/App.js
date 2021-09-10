115a2112ef69e35f231fffcf9c238f82a616cdcb
malhill <malcolmhc@msn.com>
2021-09-10 13:20:27 -0400

    added email, link to Github

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ export default function App() {
       <div>
         <Header />
         <Route exact path="/" component={Home} />
+        {/* <Route exact path='/home' component={Home} /> */}
         <Route exact path="/contact" component={Contact} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Footer />
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -8,28 +8,30 @@ export default function Contact() {
   return (
   <div className='page-height'>
 
-    <h1 className="welcome text-center my-3 ">Contact Me!</h1>
+    <h1 className='welcome text-center mb-0 my-3'>Contact Me!</h1>
+    {/* Can use my-4 for spacing above header away from the navbar */}
 
-    <div className="container">
+    <p className='welcome text-center p-1'>Email: mah.319.hc@gmail.com</p>
 
-      <div className="row" style={{ textAlign: 'center' }}>
+    <div className='container'>
 
-        <div className="col-4 order-last">
-          <a href={resume} target="_blank" className="email"><i className="fas fa-file-pdf fa-7x" /></a>
+      <div className='row' style={{ textAlign: 'center' }}>
+
+        <div className='col-4 order-last'>
+          <a title='Resume!!' href={resume} target='_blank'><i className='email fas fa-file-pdf fa-7x' /></a>
         </div>
 
-        <div className="col-4">
-          <a href="https://www.instagram.com/smooth_mt/"><i className="fab fa-instagram-square fa-7x" /></a>
+        <div className='col-4'>
+          <a title='IG page' href='https://www.instagram.com/smooth_mt/'><i className='fab fa-instagram-square fa-7x' /></a>
         </div>
 
-        <div className="col-4 order-first">
-          <a href="https://www.linkedin.com/in/malcolm-hill-0b9a24b1/"><i className="fab fa-linkedin fa-7x" /></a>
+        <div className='col-4 order-first'>
+          <a title='LinkedIn Profile'href='https://www.linkedin.com/in/malcolm-hill-0b9a24b1/'><i className='fab fa-linkedin fa-7x'/></a>
         </div>
-
       </div>
 
-      <div className="text-center">
-      <img className="img-fluid center-block" src={image} height={500} width={700} alt="Productive Programmer" id="contactimage" />
+      <div className='text-center'>
+      <img className='img-fluid center-block' src={image} height={500} width={700} alt='Productive Programmer' id='contactimage' />
       </div>
 
     </div>
